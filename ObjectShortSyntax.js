3c2b6a3991f585a40845a53069fc19c8433fa9e0
Sidenia Bezerra <72209138+sidenia@users.noreply.github.com>
2021-09-30 19:57:43 -0300

    Add files via upload

---
+++ ObjectShortSyntax.js
@@ -0,0 +1,15 @@
+// SINTAXE CURTA de objetos usando o ES6
+
+//se o nome da variavel for igual ao nome do objeto
+//nao precisa repetir duas vezes, basta declarar 1, vide nome no obj usuario
+
+const nome = 'Sidenia';
+const idade = 30;
+
+const usuario = {
+    nome,
+    idade: idade,
+    empresa: 'Itau'
+};
+
+console.log(usuario);
---
+++ templateLiterals.js
@@ -0,0 +1,7 @@
+const nome = 'Sidenia';
+const idade = 30;
+
+console.log('Meu nome é ' + nome + ' e tenho ' + idade + ' anos');
+
+//template literal
+console.log(`Meu nome é ${nome} e tenho ${idade} anos`)
